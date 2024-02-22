2e45eb6e0356360c2adfe63b64be54c77bf0f5e5
Joao Pedro de Gois Pinto <jpcic07@gmail.com>
2024-02-22 14:17:19 -0300

    initial commit

---
+++ ExemplosAula/Modulo3/TechMed.Application/InputModels/NewPacienteInputModel.cs
@@ -0,0 +1,9 @@
+namespace TechMed.Application.InputModels
+{
+    public class NewPacienteInputModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+         public string Cpf { get; set; }
+    }
+}
