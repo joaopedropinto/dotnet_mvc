66b303473a321a4967ca376e87fc9bbc22bd4dd3
Joao Pedro de Gois Pinto <jpcic07@gmail.com>
2024-02-23 14:38:01 -0300

    incomplete controller

--- ExemplosAula/Modulo5/MvcMovie/Controllers/LoginController.cs
+++ ExemplosAula/Modulo5/MvcMovie/Controllers/LoginController.cs
@@ -1,19 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
+using MvcMovie.Data.Security;
 using MvcMovie.Models;
+
 namespace MvcMovie.Controllers;
 
-public class LoginController
+public class LoginController : Controller
 {
-
+    private readonly MvcMovieContext _context;
+    public LoginController(MvcMovieContext context)
+    {
+        _context = context;
+    }
+       // GET: Login/Index
+    public IActionResult Index()
+   {
+      return View();
+   }
 }
